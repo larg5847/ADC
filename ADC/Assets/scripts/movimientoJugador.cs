d0796759046313cf615f916b4239ae83bc3b35b9
larg5847 <spartan5847@hotmail.com>
2020-02-07 02:14:33 -0600

    movimiento y primer escenario

---
+++ ADC/Assets/scripts/movimientoJugador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoJugador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.acceleration.x * Time.deltaTime,0,0);
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(transform.position.x * 3 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(-transform.position.x * 3 * Time.deltaTime, 0, 0);
+        }
+    }
+}
