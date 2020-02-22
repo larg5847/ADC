85f52e3ea05e8dbd09fa051537880b7316b82f49
josephLSalgado <49181840+josephLSalgado@users.noreply.github.com>
2020-02-21 20:40:12 -0600

    Selección de minijuegos
    Uso de panel para colocar mapa del sistema inmune, prefabs SceneFader y SelectorMinijuego

---
+++ ADC/Assets/scripts/SceneFader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneFader : MonoBehaviour
+{
+
+    //public Image img;
+    //public AnimationCurve curva;
+
+    /*void Start()
+    {
+        StartCoroutine(FadeIn());
+    }*/
+
+    public void FadeTo(string escena)
+    {
+        StartCoroutine(FadeOut(escena));
+    }
+
+    /*
+    IEnumerator FadeIn()
+    {
+        float t = 1f;
+
+        //Animación de entrada
+        while (t > 0f)
+        {
+            t -= Time.deltaTime;
+            float a = curva.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0;
+        }
+    }
+    */
+
+    IEnumerator FadeOut(string escena)
+    {
+        /*float t = 0f;
+
+        //Animación de salida
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            float a = curva.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0;
+        }*/
+        yield return 0;
+
+        SceneManager.LoadScene(escena);
+    }
+}
---
+++ ADC/Assets/scripts/SelectorMiniJuego.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorMiniJuego : MonoBehaviour
+{
+    public SceneFader fader;
+
+    public void Seleccion (string nombreMiniJuego)
+    {
+        fader.FadeTo(nombreMiniJuego);
+    }
+}
