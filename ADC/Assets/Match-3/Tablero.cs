ff690ddadbe9483381c66c351bc3f38195f45fc2
josephLSalgado <49181840+josephLSalgado@users.noreply.github.com>
2020-03-12 01:37:27 -0600

    Llenado de tablero sin match de células
    Por el momento solo se escogen entre dos células, porque faltaría modificar el prefab de virus: quitar la parte de trigger

--- ADC/Assets/Match-3/Tablero.cs
+++ ADC/Assets/Match-3/Tablero.cs
@@ -32,6 +32,18 @@ public class Tablero : MonoBehaviour
                 Vector2 posicion = new Vector2(i, j);
                 //Selección al azar de la célula del arreglo celulas
                 int indiceCelula = Random.Range(0, celulas.Length);
+                int iteraciones = 0;
+
+                //Iteración que cambia la célula a poner para que no exista
+                //match en el tablero al momento de llenarlo
+                //Máximo de 100 iteraciones para que no entre en un loop
+                //infinito
+                while(matches(i, j, celulas[indiceCelula]) && iteraciones < 100)
+                {
+                    indiceCelula = Random.Range(0, celulas.Length);
+                    iteraciones++;
+                }
+
                 GameObject backgroundTile = Instantiate(tilePrefab, posicion, Quaternion.identity) as GameObject;
                 GameObject celula = Instantiate(celulas[indiceCelula], posicion, Quaternion.identity);
 
@@ -47,6 +59,43 @@ public class Tablero : MonoBehaviour
             }
         }
     }
+
+    //Función que no deja que existan matches al momento de llenar
+    //el tablero, regresa un true si hay match
+    private bool matches(int columna, int fila, GameObject celula)
+    {
+        //Comparación de la tercera fila y columna en adelante
+        if (columna > 1 && fila > 1)
+        {
+            if (tCelulas[columna - 1, fila].tag == celula.tag &&
+                tCelulas[columna - 2, fila].tag == celula.tag)
+                return true;
+
+            if (tCelulas[columna, fila - 1].tag == celula.tag &&
+                tCelulas[columna, fila - 2].tag == celula.tag)
+                return true;
+        }
+
+        //Comparación para casos de la primera o segunda fila o columna 
+        else if(columna <= 1 || fila <=1)
+        {
+            if (columna > 1)
+            {
+                if ((tCelulas[columna - 1, fila].tag == celula.tag &&
+                tCelulas[columna - 2, fila].tag == celula.tag))
+                    return true;
+            }
+
+            if (fila > 1)
+            {
+                if ((tCelulas[columna, fila - 1].tag == celula.tag &&
+                tCelulas[columna, fila - 2].tag == celula.tag))
+                    return true;
+            }
+        }
+        return false;
+    }
+
     public int _alto
     {
         get => alto;
